f9898790f2fb7139af9d177c3fa7a13de006dab4
Hanspeter <hansdietiker2012@gmail.com>
2024-11-04 17:40:02 -0300

    fixed: handlers <editUser and Create user> implemented Promise

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -16,25 +16,29 @@ export const getUserByIdHandler = async (req: Request, res: Response) => {
         : res.status(404).json({ errorMessage: "User not Found" })
 }
 
-export const createUserHandler = async (req: Request, res: Response) => {
+export const createUserHandler = async (req: Request, res: Response): Promise<void> => {
     const { email, username, password } = req.body
 
     if (!email || !username || !password) {
-        return res.status(400).json({ errorMessage: "Insert correct Email, UserName and Password" })
+        res.status(400).json({ errorMessage: "Insert correct Email, UserName and Password" })
+        return
     }
     const user = await createUser(email, username, password)
-    return res.sendStatus(200).json({ message: "User created", user }).end()
+    res.status(200).json({ message: "User created", user })
+    return
 }
 
-export const editUserHandler = async (req: Request, res: Response) => {
+export const editUserHandler = async (req: Request, res: Response): Promise<void> => {
     const id = parseInt(req.params.id)
     const { username, password } = req.body
 
     const updateUser = await editUser(id, username, password)
     if (!updateUser) {
-        return res.status(404).json({ error: "User not found" });
+        res.status(404).json({ error: "User not found" });
+        return
     }
-    return res.sendStatus(200).json({ message: "User Updated", updateUser })
+    res.status(200).json({ message: "User Updated", updateUser })
+    return
 }
 
 
